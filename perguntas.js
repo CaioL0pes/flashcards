0fa8a22fc0d261ae1a12cb05208b9e2732ab4828
CaioLopes <caio.lopes.castro@escola.pr.gov.br>
2025-05-12 14:46:03 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Programação',
+    'Oque é JavaScript?',
+    'Linguagem de programação'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se escreve dia do pagamento?',
+    'Pay Day'
+)
+
+criaCartao(
+    'Biologia',
+    'As aves trocam de pena',
+    'Sim, e com uma certa frequência'
+)
+
