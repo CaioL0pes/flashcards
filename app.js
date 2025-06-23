1086ed1da8db7ed87ba58e00c6c0265a78a1495a
CaioLopes <caio.lopes.castro@escola.pr.gov.br>
2025-06-23 11:22:52 +0000

    ok

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+
 function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementsById('container')
+    let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
     cartao.innerHTML = `
@@ -9,29 +10,20 @@ function criaCartao(categoria, pergunta, resposta) {
                     <div class="cartao__conteudo__pergunta">
                         <p>${pergunta}</p>
                     </div>
-                    <div class="Cartao__conteudo__resposta">
-                   <p>${resposta}</p>
-                     </div>
-                </div>
-            </article>
-            <article class="cartao">
-                <div class="cartao__conteudo">
-                    <h3>Inglês</h3>
-                    <div class="cartao__conteudo__pergunta">
-                        <p>Como se fala número um em Inglês?</p>
+                    <div class="cartao__conteudo__resposta">
+                        <p>${resposta}.</p>
                     </div>
-                    <div class="Cartao__conteudo__resposta">
-                    <p>Number one</p>
-                     </div>
                 </div>
             </article>
             `
+
             let respostaEstaVisivel = false
             function viraCartao(){
                 respostaEstaVisivel = !respostaEstaVisivel
-                cartao.classList.toogle('active', respostaEstaVisivel)
+                cartao.classList.toggle('active',respostaEstaVisivel)
             }
-            cartao.addEventListener('click', viraCartao)
-            container.appenChild(cartao)
-}
+            cartao.addEventListener('click',viraCartao)
+            container.appendChild(cartao)
 
+       
+}
